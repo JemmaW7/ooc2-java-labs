4d0f513c2321c19b5fb74c88c8a6de2a1868b23c
danielcregg <daniel.cregg@gmail.com>
2019-10-09 13:08:17 +0100

    Updates

--- src/ie/gmit/ooc2labs/io/averageCalculator.java
+++ src/ie/gmit/ooc2labs/io/averageCalculator.java
@@ -9,14 +9,13 @@ public class averageCalculator {
 
 	public static void main(String args[]) throws IOException {
 
-		//try {
-			
-			File f = new File(".\\resources\\testResults.txt");
+		// try {
 
-			Scanner s = new Scanner(f);
+		File f = new File(".\\resources\\results.txt");
+		Scanner s = new Scanner(f);
 
-			int numIntsInFile = 0;
-			int total = 0;
+		int numIntsInFile = 0;
+		int total = 0;
 
 //			while (s.hasNextInt()) {
 //				intCounter++;
@@ -26,36 +25,36 @@ public class averageCalculator {
 //			System.out.println("Total results in file: " + intCounter);
 //			System.out.println("Average of all results: " + (double) total / intCounter);
 
-			// Find how many integers are in the file
-			while (s.hasNextInt()) {
-				numIntsInFile++;
-				s.nextInt();
-			}
-			
-			// Create an array to hold the ints
-			int[] arr = new int[numIntsInFile];
-			
-			//ArrayList<Integer> arrList = new ArrayList<Integer>();
-			
-			// Create a new scanner to navigate file
-			Scanner s2 = new Scanner(f);
-			for (int i = 0; i < arr.length; i++) {
-				arr[i] = s2.nextInt();
-				total += arr[i];
-				//arrList.add(arr[i]);
-			}
-			
-			System.out.println("Total results in file: " + arr.length);
-			System.out.println("All Results: " + Arrays.toString(arr));
-			System.out.println("Average of all results: " + (double) total / arr.length);
-			//System.out.println("Average of all results: " + (double) total / arrList.size());
-			
-			s.close();
-			s2.close();
-		
-		
-		//} catch (Exception e) {
-		//	return null;
-		//}
+		// Find how many integers are in the file
+		while (s.hasNextInt()) {
+			numIntsInFile++;
+			s.nextInt();
+		}
+
+		// Create an array to hold the ints
+		int[] intArray = new int[numIntsInFile];
+
+		// ArrayList<Integer> arrList = new ArrayList<Integer>();
+
+		// Create a new scanner to navigate file
+		Scanner s2 = new Scanner(f);
+		for (int i = 0; i < intArray.length; i++) {
+			intArray[i] = s2.nextInt();
+			total += intArray[i];
+			// arrList.add(intArray[i]);
+		}
+
+		System.out.println("Total results in file: " + intArray.length);
+		System.out.println("All Results: " + Arrays.toString(intArray));
+		System.out.println("Average of all results: " + (double) total / intArray.length);
+		// System.out.println("Average of all results: " + (double) total /
+		// arrList.size());
+
+		s.close();
+		s2.close();
+
+		// } catch (Exception e) {
+		// return null;
+		// }
 	}
 }
