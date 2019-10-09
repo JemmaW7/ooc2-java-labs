b653d675af2625d09bc522bde6a65141d5a994da
danielcregg <daniel.cregg@gmail.com>
2019-10-09 13:46:05 +0100

    Add try catch and get rid of throws

--- src/ie/gmit/ooc2labs/io/Averager.java
+++ src/ie/gmit/ooc2labs/io/Averager.java
@@ -7,27 +7,33 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Averager {
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) {
 
 		// Instance variables
 		File file = new File(".\\resources\\results.txt");
-		Scanner scannerObject = new Scanner(file);
-		int total = 0;
-		List<Integer> listOfInts = new ArrayList<Integer>(); // Create an array to hold the ints
-		int nextInt;
+		try {
+			Scanner scannerObject = new Scanner(file);
 
-		// Loop over ints in file
-		while (scannerObject.hasNextInt()) {
-			nextInt = scannerObject.nextInt();
-			listOfInts.add(nextInt);
-			total += nextInt;
+			int total = 0;
+			// Create an array to hold the ints
+			List<Integer> listOfInts = new ArrayList<Integer>();
+			int nextInt;
+
+			// Loop over ints in file
+			while (scannerObject.hasNextInt()) {
+				nextInt = scannerObject.nextInt();
+				listOfInts.add(nextInt);
+				total += nextInt;
+			}
+
+			System.out.println("Inputted ints:" + listOfInts.toString());
+			System.out.println("Total results in file: " + listOfInts.size());
+			System.out.println("Average of all results: " + (double) total / listOfInts.size());
+
+			scannerObject.close();
+		} catch (FileNotFoundException fnf) {
+			fnf.printStackTrace();
 		}
-
-		System.out.println("Inputted ints:" + listOfInts.toString());
-		System.out.println("Total results in file: " + listOfInts.size());
-		System.out.println("Average of all results: " + (double) total / listOfInts.size());
-
-		scannerObject.close();
 
 	}
 }
