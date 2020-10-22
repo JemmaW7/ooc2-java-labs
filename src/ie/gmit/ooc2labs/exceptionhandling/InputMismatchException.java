e7700f843e8df6652e015e3808b312dd195605a4
Daniel Cregg <daniel.cregg@gmit.ie>
2020-10-23 00:36:11 +0100

    Simplified

--- src/ie/gmit/ooc2labs/exceptionhandling/InputMismatchException.java
+++ src/ie/gmit/ooc2labs/exceptionhandling/InputMismatchException.java
@@ -2,16 +2,15 @@ package ie.gmit.ooc2labs.exceptionhandling;
 
 import java.util.Scanner;
 import java.util.InputMismatchException;
-//import java.lang.ArithmeticException;
 
-public class DivideByZero {
+public class InputMismatchException {
 
 	public static void main(String args[]) {
 		// Instance Variables
 		Scanner userInput = null;
 		int numerator = 0;
 		int denominator = 0;
-		int result = 0; // Needs to be int to generate ArithmeticException
+		float result = 0; // Needs to be int to generate ArithmeticException
 
 			try {
 				userInput = new Scanner(System.in); // Create scanner object
@@ -21,17 +20,13 @@ public class DivideByZero {
 				// Get denominator
 				System.out.print("Please enter an integer denominator: ");
 				denominator = userInput.nextInt();
-				result = numerator / denominator;
+				result = (float) (numerator / denominator);
 				// Print Result
 				System.out.println("\nResult: " + result);
 			} catch (InputMismatchException inputMisEx) {
 				System.out.println("\nYou did not enter an integer!");
 				System.out.print("Exception: " + inputMisEx);
-				System.out.println("\nPlease try again!\n");
-			} catch (ArithmeticException ae) {
-				System.out.print("\nThere was an Arithmetic Error: ");
-				System.out.print("Exception: " + ae);
-				System.out.println("\nPlease check your input numbers and try again!\n");
+				System.out.println("\nPlease try again!\n");;
 			} finally {
 				userInput.close(); // Close Scanner object
 			}
