1ab86b8141c3a4e0a312a0db0651ffb46b6bdcec
Daniel Cregg <daniel.cregg@gmit.ie>
2020-10-23 00:23:25 +0100

    Rewrite to make simpler and more to point

--- src/ie/gmit/ooc2labs/exceptionhandling/DivideByZero.java
+++ src/ie/gmit/ooc2labs/exceptionhandling/DivideByZero.java
@@ -2,40 +2,42 @@ package ie.gmit.ooc2labs.exceptionhandling;
 
 import java.util.Scanner;
 import java.util.InputMismatchException;
+//import java.lang.ArithmeticException;
 
 public class DivideByZero {
+
 	public static void main(String args[]) {
-		Scanner userInput = new Scanner(System.in); // Create scanner object
+		// Instance Variables
+		Scanner userInput = null;
 		int numerator = 0;
 		int denominator = 0;
-		boolean continueAsking = true; // determines if more input is needed
-		
-		do 
-		{
+		int result = 0; // Needs to be int to generate ArithmeticException
+
 			try {
+				userInput = new Scanner(System.in); // Create scanner object
 				// Get numerator
-				System.out.print("Please enter an integer numerator: ");     
+				System.out.print("Please enter an integer numerator: ");
 				numerator = userInput.nextInt();
 				// Get denominator
 				System.out.print("Please enter an integer denominator: ");
 				denominator = userInput.nextInt();
+				result = numerator / denominator;
 				// Print Result
-				System.out.println("\nResult: " + (float) numerator/denominator);
-				continueAsking = false; // input successful; end looping        
+				System.out.println("\nResult: " + result);
 			} catch (InputMismatchException inputMisEx) {
-				System.out.print("\nYou did not enter an integer: ");
-				System.out.print( "Exception: " + inputMisEx );
+				System.out.println("\nYou did not enter an integer!");
+				System.out.print("Exception: " + inputMisEx);
 				System.out.println("\nPlease try again!\n");
-				userInput.nextLine(); // discard input so user can try again    
-			} catch(ArithmeticException ae) {
+			} catch (ArithmeticException ae) {
 				System.out.print("\nThere was an Arithmetic Error: ");
-				System.out.print( "Exception: " + ae );
+				System.out.print("Exception: " + ae);
 				System.out.println("\nPlease check your input numbers and try again!\n");
 			} finally {
 				userInput.close(); // Close Scanner object
 			}
-		} while (continueAsking);
 		
-	} // end main
-} // end class DivideByZero
+		System.out.println("Program continues on...");
+		System.out.println("Program ending gracefully...");
 
+	} // end main
+} // end Class
