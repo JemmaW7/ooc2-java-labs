3a1aa1afc47f36c05021b58a8d6790632f31694c
Daniel Cregg <daniel.cregg@gmit.ie>
2020-10-29 12:08:50 +0000

    Added basic version of copy

---
+++ src/ie/gmit/ooc2labs/io/CopyFileByteSimple.java
@@ -0,0 +1,25 @@
+package ie.gmit.ooc2labs.io;
+
+import java.io.*;
+
+public class CopyFileByteSimple {
+	public static void main(String args[]) throws IOException {
+
+		// Create stream references
+		FileInputStream byteInputStream = new FileInputStream(".\\resources\\input.txt");
+		FileOutputStream byteOutputStream = new FileOutputStream(".\\resources\\output.txt");
+		// Create int to read and write bytes
+		int b = 0;
+
+		// Read in bytes. NOTE: If no byte is available because the end of the stream
+		// has been reached, the returned value is -1.
+		while ((b = byteInputStream.read()) != -1) {
+			byteOutputStream.write(b); // Write out bytes
+		}
+		
+		// Close streams
+		byteInputStream.close();
+		byteOutputStream.close();
+
+	} // End main
+} // End Class
