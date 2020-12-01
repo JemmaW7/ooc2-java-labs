5c31d52d1e24f8803555a53e37a1e34503747fdc
Daniel Cregg <daniel.cregg@gmit.ie>
2020-12-01 13:43:46 +0000

    Added try/catch and cleaned up

--- src/ie/gmit/ooc2labs/args/ConvertArgsFormat.java
+++ src/ie/gmit/ooc2labs/args/ConvertArgsFormat.java
@@ -1,25 +1,35 @@
 package ie.gmit.ooc2labs.args;
 
 class ConvertArgsFormat {
-	
+
 	public static void main(String args[]) {
 		// Create three integers
 		int a = 0;
 		int b = 0;
 		int c = 0;
-		
-		// Check if more than two command line arguments were entered
-		if (args.length > 2) {
-			// Convert the first two command line arguments to integers and store
-			a = Integer.parseInt(args[0]);
-			b = Integer.parseInt(args[1]);
-		}
-		
-		// Sum the stored command line arguments
-		c = a + b; 
-		
-		// Print the result to the console
-		System.out.println("The sum of " + a + " and " + b + " is: " + c);
+
+		// Check if two or more command line arguments were entered
+		if (args.length >= 2) {
+			
+			System.out.println("1st argument:\t" + args[0]);
+			System.out.println("2nd argument:\t" + args[1]);
+
+			// Check if command line arguments can be converted to integers
+			try {
+				// Convert the first two command line arguments to integers and store
+				a = Integer.parseInt(args[0]);
+				b = Integer.parseInt(args[1]);
+				// Sum the stored command line arguments
+				c = a + b;
+				// Print the result to the console
+				System.out.println(a + " + " + b + " = " + c);
+			} catch (NumberFormatException e) {
+				System.out.println("Please enter integers for the first two command line arguments!\n");
+				e.printStackTrace();
+			} // End try/catch
+			
+		} // End if
+
 	} // End main method
-	
+
 }// End Main class
