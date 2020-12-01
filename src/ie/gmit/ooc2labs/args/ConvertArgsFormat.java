5a677b793275ad479d01fc613ee58fef8f1feb29
Daniel Cregg <daniel.cregg@gmit.ie>
2020-12-01 13:18:10 +0000

    Clean code

--- src/ie/gmit/ooc2labs/args/ConvertArgsFormat.java
+++ src/ie/gmit/ooc2labs/args/ConvertArgsFormat.java
@@ -1,10 +1,19 @@
 package ie.gmit.ooc2labs.args;
 
 class ConvertArgsFormat {
+	
 	public static void main(String args[]) {
-		int a = Integer.parseInt(args[0]);
-		int b = Integer.parseInt(args[1]);
-		int c = a + b;
+		int a, b, c;
+		
+		// Convert the first two command line arguments to integers and store
+		a = Integer.parseInt(args[0]);
+		b = Integer.parseInt(args[1]);
+		
+		// Sum the stored command line arguments
+		c = a + b; 
+		
+		// Print the result to the console
 		System.out.println("The sum of " + a + " and " + b + " is: " + c);
-	} // end of main
-}// end of class
+	} // End main method
+	
+}// End Main class
