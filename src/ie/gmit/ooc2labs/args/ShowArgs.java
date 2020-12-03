2580b1bf4a0b8a322629e1c5ab71c86431cbd870
Daniel Cregg <daniel.cregg@gmit.ie>
2020-12-03 12:06:52 +0000

    added add method

--- src/ie/gmit/ooc2labs/args/ShowArgs.java
+++ src/ie/gmit/ooc2labs/args/ShowArgs.java
@@ -2,18 +2,20 @@ package ie.gmit.ooc2labs.args;
 
 public class ShowArgs {
 	public static void main(String[] args) {
-		
+
 		System.out.println("The following command line arguments were passed:");
-		
+
 		for (int i = 0; i < args.length; i++) {
 			System.out.println("arg[" + i + "]: " + args[i]);
 		}
-		
-		add(5);
-		
-	} //End main
-	
+
+		add(5); // Passing the integer 5 as an argument to this method.
+
+	} // End main
+
+	// add method with one input parameter, an integer named x.
 	static void add(int x) {
 		System.out.println(x + " + 10 = " + 10);
-	}
-} //End class
+	} // End add
+	
+} // End class
