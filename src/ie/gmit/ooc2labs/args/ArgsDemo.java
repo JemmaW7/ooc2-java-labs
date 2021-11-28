40e71378a7272f3008c3e9987a2f45e67600776a
dcregg <dcregg@GMITAZ-JMP73D3>
2021-11-28 22:17:32 +0000

    Clean up code

--- src/ie/gmit/ooc2labs/args/ArgsDemo.java
+++ src/ie/gmit/ooc2labs/args/ArgsDemo.java
@@ -3,27 +3,40 @@ package ie.gmit.ooc2labs.args;
 public class ArgsDemo {
 
 	public static void main(String[] args) {
-		//System.out.println("The args object ID is " + args);
-		System.out.println("The number of args is " + args.length);
 
-		System.out.println("The following command line arguments were passed:");
-		for (int i = 0; i < args.length; i++) {
+		// Print the number of arguments passed to this program
+		System.out.println("The number of arguments passed to this program is " + args.length + ".");
 
-			if (args[i].equals("GMIT")) {
-			System.out.println(args[i] + " ROCKS!");
-			} else {
-				System.out.println(args[i]);
-			}
-		}
-		
-		System.out.println("The Arguments backwards are:");
-		for (int x = args.length - 1; x >= 0; x--) {
-			System.out.println(args[x]);
-		}
-		
-		System.out.println("Every Second Argument is:");
-		for (int i = 0; i < args.length; i+=2) {
-			System.out.println(args[i]);
-		}
-	}
-}
+		// Print the arguments passed to this program
+//		System.out.println("The following command line arguments were passed:");
+//		for (int i = 0; i < args.length; i++) {
+//			// Print arguments
+//			System.out.println(args[i]);
+//			// Print arguments and display argument counter
+//			// System.out.println("Argument "+ (i + 1) + ": " + args[i]);
+//		}
+
+		// Print ' ROCKS!' after every GMIT arguments passed to this program
+//		System.out.println("Searching for GMIT argument...");
+//		for (int i = 0; i < args.length; i++) {
+//			if (args[i].equals("GMIT")) {
+//				System.out.println("Argument "+ (i + 1) + ": " + args[i] + " ROCKS!");
+//			} else {
+//				System.out.println("Argument "+ (i + 1) + ": " + args[i]);
+//			}
+//		}
+
+		// Print the arguments passed to this program in reverse order
+//		System.out.println("The Arguments backwards are:");
+//		for (int x = args.length - 1; x >= 0; x--) {
+//			System.out.println(args[x]);
+//		}
+
+		// Print every second argument passed to this program
+//		System.out.println("Every Second Argument is:");
+//		for (int i = 0; i < args.length; i+=2) {
+//			System.out.println(args[i]);
+//		}
+
+	} // End main
+} // End class
