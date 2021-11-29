0d37678483ab438942f56d686b3e7a36a5a39019
dcregg <dcregg@GMITAZ-JMP73D3>
2021-11-29 21:35:59 +0000

    Rename class

--- src/ie/gmit/ooc2labs/args/MethodArgs.java
+++ src/ie/gmit/ooc2labs/args/MethodArgs.java
@@ -1,20 +1,20 @@
 package ie.gmit.ooc2labs.args;
 
-public class ShowArgs {
+public class MethodArgs {
 	
 	public static void main(String[] args) {
 		
 		// Passing the integers 5 and 6 as arguments to this method.
-		add10(5, 6); 
+		add(5, 6); 
 				
 	} // End main
 
 	// add method with two input parameters, one integer named x and one named y.
-	static void add10(int x, int y) {
+	static void add(int x, int y) {
 		System.out.println("Argument 1: " + x);
 		System.out.println("Argument 2: " + y);
 		System.out.println("Argument 1 + Argument 2 = " + (x + y));
 	
 	} // End add method
-
+	
 } // End class
